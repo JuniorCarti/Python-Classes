3b6dd4035927c2aa0debc2793d9152a99c6dcb87
JuniorCarti <ridgejunior204@gmail.com>
2025-03-12 00:00:47 +0300

    Ternary Operator

--- Lesson 2.py
+++ Lesson 2.py
@@ -1,7 +1,10 @@
 meaning = 42
 print('')
 
-if meaning > 10:
-    print('Right on!')
-else:
-    print('Not Today')
+# if meaning > 10:
+#     print('Right on!')
+# else:
+#     print('Not Today')
+
+# Ternary Operator
+print('Right on!') if meaning > 10 else print('Not Today')
